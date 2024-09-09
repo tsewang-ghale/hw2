4b8c8a2989cb67f080184a24ead54b97db475ce1
tsewang-ghale <Tsewang.g@ou.edu>
2024-09-09 14:22:17 -0500

    Update post-result.php

--- post-result.php
+++ post-result.php
@@ -2,10 +2,20 @@
 $pageTitle = "Post result"; 
 include "view_header.php"; 
 ?> 
-    <h1>Post!</h1>
+    <h1>Post result </h1>
     <form method = "post" action = "postresult.php" > </form> 
     <input type = "Text" name = "" name= my_Name"> 
     <input type = "submit" value = "Post data"
 <?php
+if (isset($_POST['my_name'])) {
+?>
+    <p> The value sent is:  </p>
+<?php
+    echo $_POST['my_name']; 
+} else {
+    ?> 
+    <p> Nothing posted to the page </p>
+<?php 
+}
 include "view_footer.php";
 ?> 
