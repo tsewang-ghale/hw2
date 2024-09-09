d9e7fedbae629de437b8b805ccb28bc09a25410e
tsewang-ghale <Tsewang.g@ou.edu>
2024-09-09 13:43:51 -0500

    Create post.php

---
+++ post.php
@@ -0,0 +1,11 @@
+<?php
+$pageTitle = "Post"; 
+include "view_header.php"; 
+?> 
+    <h1>Post!</h1>
+    <form method = "post" action = "postresult.php" > </form> 
+    <input type = "Text" name = "" name= my_Name"> 
+    <input type = "submit" value = "Post data"
+<?php
+include "view_footer.php";
+?> 
