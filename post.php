0ff800f3000e3f46b0117943e27898512dbfda5b
tsewang-ghale <Tsewang.g@ou.edu>
2024-09-15 18:22:45 -0500

    Update post.php

--- post.php
+++ post.php
@@ -3,8 +3,8 @@ $pageTitle = "Post";
 include "view_header.php"; 
 ?> 
     <h1>Post!</h1>
-    <form method = "post" action = "post-result.php" > </form> 
-    <input type = "Text" name = "" name= my_Name"> 
+    <form method = "post" action= "post-result.php" > </form> 
+    <input type = "Text" name = "name= my.name"> 
     <input type = "submit" value = "Post data"
 <?php
 include "view_footer.php";
