e452aebb47c6994dbbc538ebd6aee929f25a841c
tsewang-ghale <Tsewang.g@ou.edu>
2024-09-15 23:01:31 -0500

    Update post.php

--- post.php
+++ post.php
@@ -1,15 +1,14 @@
 <?php
-$pageTitle = "Post"; 
-include "view_header.php"; 
-?> 
-    <h1>Post!</h1>
+$pageTitle = "Post";
+include "view_header.php";
+?>
+    <h1>Post Data</h1>
     <form method="post" action="post-result.php">
-        <!-- The input fields should be inside the form tags -->
         <div class="mb-3">
             <label for="name" class="form-label">Name</label>
             <input type="text" id="name" name="my_name" class="form-control" placeholder="Enter your name">
         </div>
-        <button type="submit" class="btn btn-primary">Post Data</button>
+        <button type="submit" class="btn btn-primary">Submit</button>
     </form>
 <?php
 include "view_footer.php";
