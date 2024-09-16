4e5ea3c7c0ba6339340d84da2db62a86d60ab5a4
tsewang-ghale <Tsewang.g@ou.edu>
2024-09-15 23:04:16 -0500

    Update view_footer.php

--- view_footer.php
+++ view_footer.php
@@ -1,9 +1,8 @@
- <footer class="bg-light py-3 mt-auto">
+    <footer class="bg-light py-3 mt-auto">
         <div class="container">
             <p class="text-center mb-0">&copy; <?= date("Y"); ?> Your Company Name. All rights reserved.</p>
         </div>
     </footer>
-
-    <!-- Close body and html tags -->
-    </body>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+  </body>
 </html>
