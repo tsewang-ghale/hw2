3f2224b3058e4fe7f1c968602814cb9818ef14e7
tsewang-ghale <Tsewang.g@ou.edu>
2024-09-15 23:19:37 -0500

    Update view_footer.php

--- view_footer.php
+++ view_footer.php
@@ -1,8 +1,7 @@
-    <footer class="bg-light py-3 mt-auto">
-        <div class="container">
-            <p class="text-center mb-0">&copy; <?= date("Y"); ?> </p>
-        </div>
-    </footer>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-  </body>
+<footer class="bg-light py-3 mt-auto">
+    <div class="container">
+    </div>
+</footer>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
 </html>
