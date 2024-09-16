deb95ba42c48b6794e957bf0e1d448258486e0e7
tsewang-ghale <Tsewang.g@ou.edu>
2024-09-15 23:47:06 -0500

    Update get.php

--- get.php
+++ get.php
@@ -1,14 +1,11 @@
-<?php
+ <?php
 $pageTitle = "Get";
 include "view_header.php";
 ?>
     <h1>Get Data</h1>
     <form method="get" action="get-result.php">
-        <div class="mb-3">
-            <label for="name" class="form-label">Name</label>
-            <input type="text" id="name" name="my_name" class="form-control" placeholder="Enter your name">
-        </div>
-        <button type="submit" class="btn btn-primary">Submit</button>
+            <input type="text" id="name" name="my_name">
+            <input type="submit" value = "Get data">
     </form>
 <?php
 include "view_footer.php";
