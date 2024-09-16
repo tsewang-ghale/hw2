d14aa2ece2464b56bba9b438fe5acefe7f98bced
tsewang-ghale <Tsewang.g@ou.edu>
2024-09-15 23:44:01 -0500

    Update post.php

--- post.php
+++ post.php
@@ -1,14 +1,11 @@
-<?php
+ <?php
 $pageTitle = "Post";
 include "view_header.php";
 ?>
     <h1>Post Data</h1>
     <form method="post" action="post-result.php">
-        <div class="mb-3">
-            <label for="name" class="form-label">Name</label>
-            <input type="text" id="name" name="my_name" class="form-control" placeholder="Enter your name">
-        </div>
-        <button type="submit" class="btn btn-primary">Submit</button>
+            <input type="text" id="name" name="my_name">
+            <input type="submit" value = "Post data">
     </form>
 <?php
 include "view_footer.php";
