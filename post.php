6865ec73d4205c291b561a5726cd33c35b699177
tsewang-ghale <Tsewang.g@ou.edu>
2024-09-15 23:52:20 -0500

    Update post.php

--- post.php
+++ post.php
@@ -4,7 +4,7 @@ include "view_header.php";
 ?>
     <h1>Post Data</h1>
     <form method="post" action="post-result.php">
-            <input type="text" id="name" name="my_name">
+            <input type="text" name="my_name">
             <input type="submit" value = "Post data">
     </form>
 <?php
