cd46e726b8c0ef6c9c187772b483827198b5ed00
tsewang-ghale <Tsewang.g@ou.edu>
2024-12-03 15:51:53 -0600

    Create view-idol-groups.php

---
+++ view-idol-groups.php
@@ -0,0 +1,29 @@
+<h1>Idol Groups</h1>
+<div class="table-responsive">
+  <table class="table">
+    <thead>
+      <tr>
+        <th> ID </th>
+        <th> Group Name </th>
+        <th> Debut Date </th>
+        <th> Members Count </th>
+        <th> </th>
+      </tr>
+    </thead>
+    <tbody> 
+<?php
+while ($idolGroup = $idolGroups->fetch_assoc()) {
+?>
+  <tr>
+    <td><?php echo $idolGroup['group_id']; ?> </td>
+    <td><?php echo $idolGroup['group_name']; ?></td>
+    <td><?php echo $idolGroup['debut_date']; ?></td> 
+    <td><?php echo $idolGroup['members_count']; ?></td>
+    <td><a href="idol-group-details.php?id=<?php echo $idolGroup['group_id']; ?>">Details</a></td>
+  </tr>
+<?php
+}
+?>
+    </tbody>
+  </table>
+</div>
