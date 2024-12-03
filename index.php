d76daeb9096efb87b2e244ec179975c076d7fd6a
tsewang-ghale <Tsewang.g@ou.edu>
2024-12-03 15:20:22 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,41 +1,53 @@
-<?php
-$pageTitle = "Korean Music and Idols";
-include "view_header.php";
-?>
-<div style="text-align: center; margin: 20px;">
-    <h1>Welcome to the World of K-Pop</h1>
-    <p>Explore your favorite Korean songs and idols here!</p>
-</div>
-<div style="display: flex; justify-content: space-around; margin: 20px;">
-    <div style="width: 45%; text-align: left;">
-        <h2>Korean Songs</h2>
-        <ul>
-            <li><a href="song.php?song=Butter">Butter - BTS</a></li>
-            <li><a href="song.php?song=Ice_Cream">Ice Cream - BLACKPINK feat. Selena Gomez</a></li>
-            <li><a href="song.php?song=Kill_This_Love">Kill This Love - BLACKPINK</a></li>
-            <li><a href="song.php?song=Dynamite">Dynamite - BTS</a></li>
-            <li><a href="song.php?song=Any_Song">Any Song - ZICO</a></li>
-        </ul>
-    </div>
-    <div style="width: 45%; text-align: left;">
-        <h2>Korean Idols</h2>
-        <ul>
-            <li><a href="idol.php?idol=BTS">BTS</a></li>
-            <li><a href="idol.php?idol=BLACKPINK">BLACKPINK</a></li>
-            <li><a href="idol.php?idol=TWICE">TWICE</a></li>
-            <li><a href="idol.php?idol=IU">IU</a></li>
-            <li><a href="idol.php?idol=EXO">EXO</a></li>
-        </ul>
-    </div>
-</div>
-<div style="text-align: center; margin: 20px;">
-    <h3>Want to add more?</h3>
-    <p>
-        <a href="add_song.php">Add a New Song</a> | 
-        <a href="add_idol.php">Add a New Idol</a>
-    </p>
-</div>
-<?php
-include "view_footer.php";
-?>
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title><?= htmlspecialchars($pageTitle); ?></title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  </head>
+  <body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">K-Pop World</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <a class="nav-link" href="index.php">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="idol-groups.php">Idol Groups</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="songs.php">Songs</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="awards.php">Awards</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="companies.php">Companies</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="idol-chart.php">Idol Chart</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="song-chart.php">Song Chart</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="award-chart.php">Awards Chart</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="company-chart.php">Company Chart</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
 
+    <!-- Add content for the K-Pop site below -->
+    
+  </body>
+</html>
