91bcffeb3bb62393b320dab33c265a5f0e87051f
tsewang-ghale <Tsewang.g@ou.edu>
2024-12-03 15:56:41 -0600

    Create util-db.php

---
+++ util-db.php
@@ -0,0 +1,12 @@
+<?php
+function get_db_connection(){
+    // Create connection
+    $conn = new mysqli('138.197.17.168', 'tsewangg_finalprojectuser', 'tsewang@1103', 'tsewangg_finalproject');
+    
+    // Check connection
+    if ($conn->connect_error) {
+      return false;
+    }
+    return $conn;
+}
+?>
