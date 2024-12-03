2f27005818c9a6d652d23dcc7bf193ed1e806fb3
tsewang-ghale <Tsewang.g@ou.edu>
2024-12-03 15:48:14 -0600

    Create model-companies.php

---
+++ model-companies.php
@@ -0,0 +1,15 @@
+<?php
+function selectCompanies() {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT company_id, company_name, establishment_year, location FROM Companies");
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+?>
