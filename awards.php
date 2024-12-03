3721053422fc2986b0b1387d2a904f3112e1459b
tsewang-ghale <Tsewang.g@ou.edu>
2024-12-03 15:23:50 -0600

    Create awards.php

---
+++ awards.php
@@ -0,0 +1,10 @@
+<?php
+require_once("util-db.php");
+require_once("model-awards.php");
+
+$pageTitle = "Awards";
+include "view-header.php";
+$awards = selectAwards();  // Fetch awards from the model
+include "view-awards.php";  // Display awards
+include "view-footer.php";
+?>
