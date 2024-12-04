c0a932e5a10196d208d5b79750f4605fffa3ef23
tsewang-ghale <Tsewang.g@ou.edu>
2024-12-04 12:19:48 -0600

    Update view-awards.php

--- view-awards.php
+++ view-awards.php
@@ -17,7 +17,7 @@ while ($award = $awards->fetch_assoc()) {
   <tr>
     <td><?php echo $award['award_id']; ?> </td>
     <td><?php echo $award['award_name']; ?></td>
-    <td><?php echo $award['award_year']; ?></td> 
+    <td><?php echo $award['award_date']; ?></td> 
     <td><?php echo $award['idol_group_id']; ?></td>
     <td><a href="award-details.php?id=<?php echo $award['award_id']; ?>">Details</a></td>
   </tr>
