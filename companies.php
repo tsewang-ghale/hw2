ccbc6be9de81e0aa8b098a02ddcffc9a4e8f7eb8
tsewang-ghale <Tsewang.g@ou.edu>
2024-12-04 11:59:53 -0600

    Update companies.php

--- companies.php
+++ companies.php
@@ -3,8 +3,8 @@ require_once("util-db.php");
 require_once("model-companies.php");
 
 $pageTitle = "Companies";
-include "view-header.php";
+include "view_header.php";
 $companies = selectCompanies();  // Fetch companies from the model
 include "view-companies.php";  // Display companies
-include "view-footer.php";
+include "view_footer.php";
 ?>
