9390295f440153e78e2fa7cceb742d8417509def
tsewang-ghale <Tsewang.g@ou.edu>
2024-12-04 11:59:37 -0600

    Update awards.php

--- awards.php
+++ awards.php
@@ -3,8 +3,8 @@ require_once("util-db.php");
 require_once("model-awards.php");
 
 $pageTitle = "Awards";
-include "view-header.php";
+include "view_header.php";
 $awards = selectAwards();  // Fetch awards from the model
 include "view-awards.php";  // Display awards
-include "view-footer.php";
+include "view_footer.php";
 ?>
