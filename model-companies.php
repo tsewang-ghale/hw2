fcc9e5ec278e1420c1422700a694473fe569d2d4
tsewang-ghale <Tsewang.g@ou.edu>
2024-12-04 12:02:01 -0600

    Update model-companies.php

--- model-companies.php
+++ model-companies.php
@@ -2,7 +2,7 @@
 function selectCompanies() {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT company_id, company_name, establishment_year, location FROM Companies");
+        $stmt = $conn->prepare("SELECT company_id, company_name,company_foundation_date, company_ceo FROM Companies");
         $stmt->execute();
         $result = $stmt->get_result();
         $conn->close();
