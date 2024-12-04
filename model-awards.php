f9ce288b351552cd7abed2bc8d3b316abad4e92b
tsewang-ghale <Tsewang.g@ou.edu>
2024-12-04 12:19:12 -0600

    Update model-awards.php

--- model-awards.php
+++ model-awards.php
@@ -2,7 +2,7 @@
 function selectAwards() {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT award_id, award_name, award_year, idol_group_id FROM Awards");
+        $stmt = $conn->prepare("SELECT award_id, award_name, award_date, idol_group_id FROM Awards");
         $stmt->execute();
         $result = $stmt->get_result();
         $conn->close();
