08a1566c25cdbcaa2de5bbb04af36de5b05387ac
tsewang-ghale <Tsewang.g@ou.edu>
2024-12-04 12:00:24 -0600

    Update songs.php

--- songs.php
+++ songs.php
@@ -3,8 +3,8 @@ require_once("util-db.php");
 require_once("model-songs.php");
 
 $pageTitle = "Songs";
-include "view-header.php";
+include "view_header.php";
 $songs = selectSongs();  // Fetch songs from the model
 include "view-songs.php";  // Display songs
-include "view-footer.php";
+include "view_footer.php";
 ?>
