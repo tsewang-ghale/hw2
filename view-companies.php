fabfcc0f68fbaba92e940379a95bc139a0a34e94
tsewang-ghale <Tsewang.g@ou.edu>
2024-12-04 12:18:19 -0600

    Update view-companies.php

--- view-companies.php
+++ view-companies.php
@@ -17,7 +17,7 @@ while ($company = $companies->fetch_assoc()) {
   <tr>
     <td><?php echo $company['company_id']; ?> </td>
     <td><?php echo $company['company_name']; ?></td>
-    <td><?php echo $company['establishment_year']; ?></td> 
+    <td><?php echo $company['company_foundation_date']; ?></td> 
     <td><?php echo $company['company_ceo']; ?></td>
     <td><a href="company-details.php?id=<?php echo $company['company_id']; ?>">Details</a></td>
   </tr>
