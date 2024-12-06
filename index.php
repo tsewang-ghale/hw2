741e2de4dd8bce2e2d83c63e99fc2f285605f968
tsewang-ghale <Tsewang.g@ou.edu>
2024-12-06 00:22:42 -0600

    Update index.php

--- index.php
+++ index.php
@@ -5,6 +5,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title><?= htmlspecialchars($pageTitle); ?></title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <style>
+      body {
+        background-image: url('k_pop.jpg');
+        background-size: cover;
+        background-position: center;
+        background-attachment: fixed;
+        color: white; /* Optional: Adjust text color for contrast */
+      }
+    </style>
   </head>
   <body>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
