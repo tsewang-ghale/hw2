b137e421590248a9b02b236e25982344ecfea827
tsewang-ghale <Tsewang.g@ou.edu>
2024-12-11 17:10:56 -0600

    Update view-idol-groups.php

--- view-idol-groups.php
+++ view-idol-groups.php
@@ -3,7 +3,6 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Idol Groups</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <style>
       body {
@@ -29,7 +28,6 @@
     <div class="container my-5">
       <div class="row">
         <div class="col">
-          <h1>Idol Groups</h1>
         </div>
         <div class="col-auto">
           <?php 
