e9b5f3e86f79c11376f0437a57177a4fc06b87c8
tsewang-ghale <Tsewang.g@ou.edu>
2024-12-10 23:39:47 -0600

    Create view-idol-group-newform.php

---
+++ view-idol-group-newform.php
@@ -0,0 +1,42 @@
+<!-- Button trigger modal -->
+<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#newIdolGroupModal">
+  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-circle" viewBox="0 0 16 16">
+    <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
+    <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4"/>
+  </svg>
+</button>
+
+<!-- Modal -->
+<div class="modal fade" id="newIdolGroupModal" tabindex="-1" aria-labelledby="newIdolGroupModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-5" id="newIdolGroupModalLabel">New Idol Group</h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <form method="post" action="">
+          <div class="mb-3">
+            <div class="mb-3">
+            <label for="idol_group_id" class="form-label">Idol Group ID</label>
+            <input type="number" class="form-control" id="idol_group_id" name="idol_group_id">
+          </div>
+            <label for="group_name" class="form-label">Group Name</label>
+            <input type="text" class="form-control" id="group_name" name="group_name">
+          </div>
+          
+          <div class="mb-3">
+            <label for="debut_year" class="form-label">Debut Year</label>
+            <input type="date" class="form-control" id="debut_year" name="debut_year">
+          </div>
+          <div class="mb-3">
+            <label for="members_count" class="form-label">Members Count</label>
+            <input type="number" class="form-control" id="members_count" name="members_count">
+          </div>
+          <input type="hidden" name="actionType" value="Add">
+          <button type="submit" class="btn btn-primary">Save</button>
+        </form>
+      </div>
+    </div>
+  </div>
+</div>
