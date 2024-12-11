08efaffe34bae7e65c9268563c8e2bc83a773ef8
tsewang-ghale <Tsewang.g@ou.edu>
2024-12-11 16:31:32 -0600

    Update view-awards.php

--- view-awards.php
+++ view-awards.php
@@ -7,7 +7,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <style>
       body {
-        background-image: url('b4.jpg');
+        background-image: url('b3.jpg');
         background-size: cover;
         background-position: center;
         background-attachment: fixed;
