2e9f84fbb3dfa670cee3ac78c72cc4efedaa46f3
tsewang-ghale <Tsewang.g@ou.edu>
2024-12-12 23:38:39 -0600

    Update songs.php

--- songs.php
+++ songs.php
@@ -8,14 +8,14 @@ include "view_header.php";
 if (isset($_POST['actionType'])) {
     switch ($_POST['actionType']) {
         case "Add":
-            if (InsertSong($_POST['song_name'], $_POST['release_date'], $_POST['idol_group_id'])) {
+            if (InsertSong($_POST['song_name'], $_POST['release_date'], $_POST['group_id'])) {
                 echo '<div class="alert alert-success" role="alert">Song added.</div>';
             } else {
                 echo '<div class="alert alert-danger" role="alert">Error adding Song.</div>';
             }
             break;
         case "Edit":
-            if (UpdateSong($_POST['song_id'], $_POST['song_name'], $_POST['release_date'], $_POST['idol_group_id'])) {
+            if (UpdateSong($_POST['song_id'], $_POST['song_name'], $_POST['release_date'], $_POST['group_id'])) {
                 echo '<div class="alert alert-success" role="alert"> Song edited.</div>';
             } else {
                 echo '<div class="alert alert-danger" role="alert">Error editing Song.</div>';
