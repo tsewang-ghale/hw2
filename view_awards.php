fa25c4e32dc2e5f660b3ca892a7629b1849e1e30
tsewang-ghale <Tsewang.g@ou.edu>
2024-12-13 00:39:42 -0600

    Update view_awards.php

--- view_awards.php
+++ view_awards.php
@@ -27,8 +27,8 @@
         <td><?php echo $award['award_id']; ?> </td>
         <td><?php echo $award['award_name']; ?></td>
         <td><?php echo $award['award_year']; ?></td> 
-        <td><?php echo $award['idol_group_id']; ?></td>
-        <td><a href="award-details.php?id=<?php echo $award['award_id']; ?>" class="btn btn-primary btn-sm">Details</a></td>
+        <td><?php echo $award['group_id']; ?></td>
+        <td></td>
         <td>
           <?php
           include "view_awards_editform.php"; 
