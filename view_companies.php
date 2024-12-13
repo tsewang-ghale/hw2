b5470215e439899176a87828a3a373b963399034
tsewang-ghale <Tsewang.g@ou.edu>
2024-12-13 02:30:30 -0600

    Update view_companies.php

--- view_companies.php
+++ view_companies.php
@@ -4,7 +4,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Companies</title>
- <!-- Bootstrap CDN -->
+  <!-- Bootstrap CDN -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
   <style>
     /* Set the background image for the entire page */
@@ -46,7 +46,6 @@
       border-color: #004085;
     }
 
-    
     .row h1 {
       font-size: 2.5rem;
       font-weight: bold;
@@ -60,65 +59,69 @@
   </style>
 </head>
 <body>
-  
-<div class="container mt-4">
+
+  <div class="container mt-4">
     <!-- Row for the Header and Form -->
     <div class="row mb-4">
       <div class="col">
-      <h1>Companies</h1>
+        <h1>Companies</h1>
+      </div>
+      <div class="col-auto">
+        <?php 
+          include "view_companies_newform.php"; 
+        ?>
+      </div>
     </div>
-    <div class="col-auto">
-      <?php 
-        include "view_companies_newform.php"; 
-      ?>
-    </div>
-  </div>
-  
-   <div class="table-responsive mt-4">
+
+    <!-- Table for displaying Companies -->
+    <div class="table-responsive mt-4">
       <table class="table table-bordered table-hover">
         <thead>
-        <tr>
-          <th>ID</th>
-          <th>Company Name</th>
-          <th>Establishment Year</th>
-          <th>Company CEO</th>
-          <th>Edit</th>
-          <th> Delete</th>
-        </tr>
-      </thead>
-      <tbody>
-      <?php
-      while ($company = $companies->fetch_assoc()) {
-      ?>
-        <tr>
-          <td><?php echo $company['company_id']; ?> </td>
-          <td><?php echo $company['company_name']; ?></td>
-          <td><?php echo $company['company_foundation_year']; ?></td> 
-          <td><?php echo $company['company_ceo']; ?></td>
-          <td>
-            <?php
-            include "view_companies_editform.php"; 
-            ?>
-          </td>
-          <td>
-            <form method="post" action="">
-              <input type="hidden" name="company_id" value="<?php echo $company['company_id']; ?>">
-              <input type="hidden" name="actionType" value="Delete">
-              <button type="submit" class="btn" onclick="return confirm('Are you sure?');">
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
-                  <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z"/>
-                  <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z"/>
-                </svg>
-              </button>
-            </form>
-          </td>
-        </tr>
-      <?php
-      }
-      ?>
-      </tbody>
-    </table>
+          <tr>
+            <th>ID</th>
+            <th>Company Name</th>
+            <th>Establishment Year</th>
+            <th>Company CEO</th>
+            <th>Edit</th>
+            <th>Delete</th>
+          </tr>
+        </thead>
+        <tbody>
+          <?php
+            while ($company = $companies->fetch_assoc()) {
+          ?>
+            <tr>
+              <td><?php echo $company['company_id']; ?></td>
+              <td><?php echo $company['company_name']; ?></td>
+              <td><?php echo $company['company_foundation_year']; ?></td>
+              <td><?php echo $company['company_ceo']; ?></td>
+              <td>
+                <?php
+                include "view_companies_editform.php";
+                ?>
+              </td>
+              <td>
+                <form method="post" action="">
+                  <input type="hidden" name="company_id" value="<?php echo $company['company_id']; ?>">
+                  <input type="hidden" name="actionType" value="Delete">
+                  <button type="submit" class="btn btn-danger" onclick="return confirm('Are you sure?');">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+                      <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z"/>
+                      <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z"/>
+                    </svg>
+                  </button>
+                </form>
+              </td>
+            </tr>
+          <?php
+            }
+          ?>
+        </tbody>
+      </table>
+    </div>
+
   </div>
+
   <!-- Bootstrap JS and dependencies -->
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
 </body>
