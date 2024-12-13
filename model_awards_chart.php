37f2d1cea031ec99129dca6e97a268bebe2c86ff
tsewang-ghale <Tsewang.g@ou.edu>
2024-12-13 03:18:28 -0600

    Update model_awards_chart.php

--- model_awards_chart.php
+++ model_awards_chart.php
@@ -1,1 +1,19 @@
 
+<?php
+function selectSongs() {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT c.group_name, COUNT(s.song_id) AS count_songs 
+                                FROM `Songs` s 
+                                JOIN `IdolGroups` c ON c.group_id = s.group_id 
+                                GROUP BY c.group_name");
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+?>
