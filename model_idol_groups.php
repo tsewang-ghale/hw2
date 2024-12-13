83b79be3036d344db1838e3097d00f5061fa79af
tsewang-ghale <Tsewang.g@ou.edu>
2024-12-12 21:38:40 -0600

    Update model_idol_groups.php

--- model_idol_groups.php
+++ model_idol_groups.php
@@ -16,7 +16,7 @@ function InsertIdolGroup($group_name, $debut_year, $members_count) {
     try {
         $conn = get_db_connection();
         $stmt = $conn->prepare("INSERT INTO `IdolGroups` (`group_name`, `debut_year`,`members_count`) VALUES (?, ?, ?)");
-        $stmt->bind_param("sii", $group_name, $debut_year, $members_count);
+        $stmt->bind_param("ssi", $group_name, $debut_year, $members_count);
         $success= $stmt->execute();
         $conn->close();
         return $success;
@@ -32,7 +32,7 @@ function UpdateIdolGroup($group_id,$group_name, $debut_year, $members_count) {
         if (!$stmt) {
             throw new Exception("Failed to prepare statement: " . $conn->error);
         }
-        $stmt->bind_param("isii",$group_id,$group_name, $debut_year, $members_count); 
+        $stmt->bind_param("issi",$group_id,$group_name, $debut_year, $members_count); 
         $success = $stmt->execute();
         $stmt->close();
         $conn->close();
