948113f0f9a4f0c38520b7eb59e005aea8acc852
tsewang-ghale <Tsewang.g@ou.edu>
2024-12-13 03:04:06 -0600

    Update songs_chart.php

--- songs_chart.php
+++ songs_chart.php
@@ -2,7 +2,7 @@
 require_once("util_db.php");
 require_once("model_songs_chart.php");
   
-$pageTitle = "Song Chart";
+$pageTitle = "Songs Chart";
 include "view_header.php";
 $customers = selectSongs();
 include "view_songs_chart.php";
