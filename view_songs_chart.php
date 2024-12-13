b28de3665b3ead01fcbd950cf4a99933a7bbb063
tsewang-ghale <Tsewang.g@ou.edu>
2024-12-13 04:21:50 -0600

    Update view_songs_chart.php

--- view_songs_chart.php
+++ view_songs_chart.php
@@ -6,7 +6,7 @@
 <style>
   /* Set the background image for the entire page or a specific container */
   body {
-    background-image: url('b4.jpeg');
+    background-image: url('b3.jpg');
     background-size: cover; /* Make sure the background covers the entire page */
     background-position: center; /* Center the background */
     background-attachment: fixed; /* Keep the background fixed when scrolling */
