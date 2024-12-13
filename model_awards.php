658c9cc3ab168005ca6921ee0f7a173235f9b232
tsewang-ghale <Tsewang.g@ou.edu>
2024-12-13 01:34:28 -0600

    Update model_awards.php

--- model_awards.php
+++ model_awards.php
@@ -16,7 +16,7 @@ function selectAwards() {
 function InsertAward($award_id, $award_name, $award_year, $group_id) {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("INSERT INTO Awards (award_id, award_name, award_year, group_id) VALUES (?, ?, ?)");
+        $stmt = $conn->prepare("INSERT INTO Awards (award_id, award_name, award_year, group_id) VALUES (?, ?, ?, ?)");
         $stmt->bind_param("isii", $award_name, $award_year, $group_id);
         $success = $stmt->execute();
         $conn->close();
