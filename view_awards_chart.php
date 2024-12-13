1984542689e78c76bc00ddad829b3c02875fa831
tsewang-ghale <Tsewang.g@ou.edu>
2024-12-13 03:42:23 -0600

    Update view_awards_chart.php

--- view_awards_chart.php
+++ view_awards_chart.php
@@ -9,7 +9,7 @@
   /* Ensure that the canvas has specific width and height */
   #myChart {
     width: 70% !important; /* Adjust the width of the chart relative to its container */
-    height: 300px !important; /* Fixed height for the chart */
+    height: 500px !important; /* Fixed height for the chart */
   }
 </style>
 
