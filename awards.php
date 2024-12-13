4c46f649085b7a2962a24a03112397a3dd226d9e
tsewang-ghale <Tsewang.g@ou.edu>
2024-12-13 00:38:23 -0600

    Update awards.php

--- awards.php
+++ awards.php
@@ -8,14 +8,14 @@ include "view_header.php";
 if (isset($_POST['actionType'])) {
     switch ($_POST['actionType']) {
         case "Add":
-            if (InsertAward($_POST['award_id'], $_POST['award_name'], $_POST['award_date'],$_POST['idol_group_id'] )) {
+            if (InsertAward($_POST['award_id'], $_POST['award_name'], $_POST['award_year'],$_POST['group_id'] )) {
                 echo '<div class="alert alert-success" role="alert">Award added.</div>';
             } else {
                 echo '<div class="alert alert-danger" role="alert">Error adding Award.</div>';
             }
             break;
         case "Edit":
-            if (UpdateAward($_POST['award_id'], $_POST['award_name'], $_POST['award_date'],$_POST['idol_group_id'])) {
+            if (UpdateAward($_POST['award_id'], $_POST['award_name'], $_POST['award_year'],$_POST['group_id'])) {
                 echo '<div class="alert alert-success" role="alert">Award edited.</div>';
             } else {
                 echo '<div class="alert alert-danger" role="alert">Error editing Award.</div>';
