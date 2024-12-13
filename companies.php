77bde9c573579d528516b10b0c64957d86755a55
tsewang-ghale <Tsewang.g@ou.edu>
2024-12-13 00:15:20 -0600

    Update companies.php

--- companies.php
+++ companies.php
@@ -8,14 +8,14 @@ include "view_header.php";
 if (isset($_POST['actionType'])) {
     switch ($_POST['actionType']) {
         case "Add":
-            if (InsertCompany($_POST['company_name'], $_POST['company_foundation_date'], $_POST['company_ceo'])) {
+            if (InsertCompany($_POST['company_name'], $_POST['company_foundation_year'], $_POST['company_ceo'])) {
                 echo '<div class="alert alert-success" role="alert">Company added.</div>';
             } else {
                 echo '<div class="alert alert-danger" role="alert">Error adding company.</div>';
             }
             break;
         case "Edit":
-            if (UpdateCompany($_POST['company_id'], $_POST['company_name'], $_POST['company_foundation_date'], $_POST['company_ceo'])) {
+            if (UpdateCompany($_POST['company_id'], $_POST['company_name'], $_POST['company_foundation_year'], $_POST['company_ceo'])) {
                 echo '<div class="alert alert-success" role="alert">Company updated.</div>';
             } else {
                 echo '<div class="alert alert-danger" role="alert">Error updating company.</div>';
