af633e71b8c180106615e7bf8af95f60b4fcfc31
tsewang-ghale <Tsewang.g@ou.edu>
2024-12-12 22:55:21 -0600

    Update songs.php

--- songs.php
+++ songs.php
@@ -1,37 +1,37 @@
 <?php
-require_once("util-db.php");
-require_once("model-songs.php");
+require_once("util_db.php");
+require_once("model_songs.php");
 
 $pageTitle = "Songs";
 include "view_header.php";
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['actionType'])) {
+if (isset($_POST['actionType'])) {
     switch ($_POST['actionType']) {
         case "Add":
             if (InsertSong($_POST['song_name'], $_POST['release_date'], $_POST['idol_group_id'])) {
-                echo '<div class="alert alert-success" role="alert">Song added successfully.</div>';
+                echo '<div class="alert alert-success" role="alert">Song added.</div>';
             } else {
-                echo '<div class="alert alert-danger" role="alert">Error adding song.</div>';
+                echo '<div class="alert alert-danger" role="alert">Error adding Song.</div>';
             }
             break;
         case "Edit":
             if (UpdateSong($_POST['song_id'], $_POST['song_name'], $_POST['release_date'], $_POST['idol_group_id'])) {
-                echo '<div class="alert alert-success" role="alert">Song updated successfully.</div>';
+                echo '<div class="alert alert-success" role="alert"> Song edited.</div>';
             } else {
-                echo '<div class="alert alert-danger" role="alert">Error updating song.</div>';
+                echo '<div class="alert alert-danger" role="alert">Error editing Song.</div>';
             }
             break;
         case "Delete":
             if (DeleteSong($_POST['song_id'])) {
-                echo '<div class="alert alert-success" role="alert">Song deleted successfully.</div>';
+                echo '<div class="alert alert-success" role="alert">Song deleted.</div>';
             } else {
-                echo '<div class="alert alert-danger" role="alert">Error deleting song.</div>';
+                echo '<div class="alert alert-danger" role="alert">Error deleting Song.</div>';
             }
             break;
     }
 }
 
 $Songs = selectSongs(); // Fetch all songs from the model
-include "view-songs.php";
+include "view_songs.php";
 include "view_footer.php"; 
 ?>
