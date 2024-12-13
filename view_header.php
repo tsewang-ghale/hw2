d674403f768f5400c538405631a9e693260374fd
tsewang-ghale <Tsewang.g@ou.edu>
2024-12-13 03:32:44 -0600

    Update view_header.php

--- view_header.php
+++ view_header.php
@@ -54,7 +54,7 @@
               <a class="nav-link" href="songs_chart.php"> Songs Chart</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="awards_char.php">  Awards Chart</a>
+              <a class="nav-link" href="awards_chart.php">  Awards Chart</a>
             </li>
             
           </ul>
